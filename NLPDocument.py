1f46fb8ea5cd6ad9e53c83430ed845ded4b866b2
lisaro82 <lisaro82@hotmail.com>
2018-11-09 00:57:58 +0100

    Initial commit

---
+++ NLPDocument.py
@@ -0,0 +1,135 @@
+import re
+from string import punctuation
+
+# Try to import the libraries for nltk. In case of error, download the necessary components and try again.
+for _ in range(2):
+    try:
+        import nltk
+        from nltk.tokenize import word_tokenize, sent_tokenize
+        from nltk.corpus import stopwords
+        from nltk.stem import WordNetLemmatizer
+        from nltk.stem.porter import PorterStemmer        
+
+        g_StopWordsEnglish = stopwords.words('english')
+        v_addWords = [ 'new', 'like', 'u', "it'", "'s", "n't", 'mr.', 'from', 'subject', 're', 'edu', 'use', 'table' ]
+        g_StopWordsEnglish.extend(v_addWords)
+        g_StopWordsEnglish = list(set(g_StopWordsEnglish))
+
+        break
+    except:
+        import nltk
+        nltk.download(['punkt', 'stopwords', 'wordnet', 'averaged_perceptron_tagger', 'words'])
+
+        
+#--------------------------------------------------------------------------------------------------------------------------------------
+class NLPDocument():
+    
+    __punctuation      = None
+    __usePorterStemmer = None
+    __filterMinSize    = None
+    __document         = None
+    __excludeTags      = None
+    __includeTags      = None
+    
+    __clean_document   = None
+    __clean_tokens     = None
+    __pos_tags         = None
+    
+    def __init__( self, p_document, 
+                  p_usePorterStemmer = True, 
+                  p_filterMinSize    = None, 
+                  p_excludeTags      = None, 
+                  p_includeTags      = None ):
+        self.__punctuation      = punctuation + '”“‘’»' + "'"
+        self.__document         = p_document.lower().replace("'ll", ' will')
+        self.__usePorterStemmer = p_usePorterStemmer    
+        self.__filterMinSize    = p_filterMinSize 
+        self.__excludeTags      = p_excludeTags 
+        self.__includeTags      = p_includeTags
+        
+        self.__wordTokenize__()        
+        return
+    
+    def getTokenizedDoc(self):
+        return self.__clean_document
+    
+    def getCollocations(self):
+        return self.__clean_document.collocations()
+    
+    def getPosTag(self):
+        return self.__pos_tags
+    
+    def printPosTag(self, p_top = 20):
+        for key, value in self.__pos_tags.items():
+            print(f'{key.ljust(6, " ")} has {len(value)} occurence(s). {value[:p_top]}')        
+        return
+    
+    def __sentTokenize__(self):
+        for item in ['¿', '--', '*']:
+            self.__document = self.__document.replace(item, ' ')
+            
+        self.__document = self.__document.replace(' www.', 'http://')
+                            
+        # Remove URLs
+        v_url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+        v_urls = re.findall(v_url_regex, self.__document)
+        for url in v_urls:
+            self.__document = self.__document.replace(url, "urlplaceholder")
+
+        # Remove Emails
+        v_email_regex = '\S*@\S*\s?'
+        v_emails = re.findall(v_email_regex, self.__document)
+        for email in v_emails:
+            self.__document = self.__document.replace(email, "emailplaceholder") 
+        
+        # Remove new line characters
+        # self.__document = re.sub('\s+', ' ', self.__document)
+        
+        return nltk.sent_tokenize(self.__document)
+    
+    def __wordTokenize__(self):
+        v_tokens = []
+        for token_by_sent in [word_tokenize(sentence) for sentence in self.__sentTokenize__()]:
+            v_tokens += token_by_sent
+            
+        v_tokens = list(filter(lambda item: item not in g_StopWordsEnglish, v_tokens))
+        v_tokens = list(filter(lambda item: item not in punctuation, v_tokens))
+        v_tokens = list(filter(lambda item: item not in [u"...", u'``', u'\u2014', u'\u2026', u'\u2013'], v_tokens))
+                        
+        self.__clean_tokens = []
+        for token in v_tokens:
+            token = WordNetLemmatizer().lemmatize(token)
+            token = WordNetLemmatizer().lemmatize(token, pos = 'v')
+            
+            if not self.__filterMinSize is None:
+                if len(token) < self.__filterMinSize:
+                    continue
+                
+            if self.__usePorterStemmer:
+                token = PorterStemmer().stem(token)
+            
+            self.__clean_tokens.append(token.strip())
+        
+        v_pos_tag = nltk.pos_tag(self.__clean_tokens)
+                
+        if not self.__includeTags is None:
+            self.__clean_tokens = [item[0] for item in v_pos_tag if item[1] in self.__includeTags]
+        
+        if not self.__excludeTags is None:
+            self.__clean_tokens = [item[0] for item in v_pos_tag if item[1] not in self.__excludeTags]
+            
+        #v_wordlist = [w for w in nltk.corpus.words.words('en') if w.islower()]
+        #v_ner_tagged = nltk.ne_chunk(v_tagged_tokens)
+        
+        self.__clean_document = ' '.join(self.__clean_tokens)
+                
+        self.__pos_tags = {}
+        for item in nltk.pos_tag(self.__clean_tokens):
+            v_key = item[1]
+            if not v_key in self.__pos_tags.keys():
+                self.__pos_tags[v_key] = []
+            self.__pos_tags[v_key].append(item[0])
+            
+        self.__pos_tags = {key: sorted(set(value)) for key, value in self.__pos_tags.items()}
+        
+        return self.__clean_document
---
+++ Recommend.py
@@ -0,0 +1,222 @@
+import pandas as pd
+import numpy as np
+
+def getArticles(p_articlesID, p_articles):
+    return p_articles.set_index('article_id').loc[p_articlesID, ['title', 'topic_keywords']].reset_index()
+
+def getTopArticlesID(p_userArticles, p_top = 20):
+    v_data = p_userArticles.groupby(['article_id']).agg({'article_id': ['count']}).reset_index()
+    v_data.columns = ['article_id', 'Article Read No']
+    return v_data.sort_values('Article Read No', ascending = False).head(p_top)
+
+def getTopArticles(p_articles, p_userArticles, p_top = 20):
+    v_TopArticlesID = getTopArticlesID( p_userArticles = p_userArticles, 
+                                        p_top          = p_top )
+    v_TopArticlesID = v_TopArticlesID.merge(p_articles, how = 'inner', on = 'article_id')    
+    return v_TopArticlesID[['article_id', 'title', 'topic_keywords', 'Article Read No']]
+
+class User():
+    
+    __user_id__              = None
+    __userArticles__         = None
+    __userArticlesNo__       = None
+    __userArticlesInteract__ = None
+    
+    def __init__(self, p_user_id, p_articles, p_userArticles, p_userArticlesMatrix):
+        self.__user_id__ = p_user_id
+        self.__setUserArticles__(p_articles, p_userArticles, p_userArticlesMatrix)
+        return
+    
+    def getUserID(self):
+        return self.__user_id__
+    
+    def getUserArticles(self):
+        return self.__userArticles__
+    
+    def getUserArticlesNo(self):
+        return self.__userArticlesNo__
+    
+    def getUserArticlesInteract(self):
+        return self.__userArticlesInteract__
+    
+    def __setUserArticles__(self, p_articles, p_userArticles, p_userArticlesMatrix):
+        v_userArticleGroup = p_userArticles.groupby(['article_id']).agg({'article_id': ['count']})
+        v_userArticleGroup.columns = ['Article Read No']
+        
+        v_userArticlesMatrix = p_userArticlesMatrix.loc[self.__user_id__, :]
+        v_userArticlesID = v_userArticlesMatrix[~v_userArticlesMatrix.isnull()].index.tolist()    
+        
+        self.__userArticlesNo__       = len(v_userArticlesID)
+        self.__userArticlesInteract__ = ( p_userArticles[p_userArticles['user_id'] == self.__user_id__]
+                                             .count().reset_index().loc[0, 0] )
+        
+        v_articles = getArticles( p_articlesID = v_userArticlesID,
+                                  p_articles   = p_articles )
+
+        v_articles = v_articles.merge(v_userArticleGroup.reset_index(), how = 'inner', on = 'article_id')
+        v_articles['user_id'] = self.__user_id__
+        
+        self.__userArticles__ = v_articles.sort_values('Article Read No', ascending = False).reset_index(drop = True)
+        return
+    
+    
+class RecommendArticles():
+    
+    __User__             = None
+    __recNumber__        = None # Number of articles to be recommended
+    __artPerUser__       = None # Number of articles selected from a particular user read articles
+    
+    __keywords__         = None
+    
+    __similarUsersID__   = None
+    __similarUsersDet__  = None
+    
+    __topArticles__      = None
+    __recommendations__  = None
+    
+    def __init__(self, p_user_id, p_articles, p_userArticles, p_userArticlesMatrix, 
+                       p_artPerUser = 10, p_top = 30, p_keywords = [] ): 
+        
+        v_articles = p_articles.append(pd.DataFrame({ 'article_id':     -999, 
+                                                      'topic_keywords': 'nan' }, index = [-1]), ignore_index = True, sort = True)
+        
+        self.__User__ = User( p_user_id            = p_user_id, 
+                              p_articles           = v_articles, 
+                              p_userArticles       = p_userArticles, 
+                              p_userArticlesMatrix = p_userArticlesMatrix )
+        self.__setSimilarUsers__( p_articles           = v_articles, 
+                                  p_userArticles       = p_userArticles, 
+                                  p_userArticlesMatrix = p_userArticlesMatrix,
+                                  p_top                = round(p_top / 2) )
+        self.__topArticles__ = getTopArticles( p_articles     = v_articles, 
+                                               p_userArticles = p_userArticles, 
+                                               p_top          = v_articles.shape[0] )
+        
+        self.__recNumber__  = p_top
+        self.__artPerUser__ = p_artPerUser
+        self.__keywords__   = p_keywords
+        self.__getRecommendations__()
+        
+        return
+            
+    def getUser(self):
+        return self.__User__
+            
+    def getSimilarUsers(self):
+        return self.__similarUsersID__
+    
+    def getSimilarUsersDetails(self, p_user_id):
+        return self.__similarUsersDet__[p_user_id]
+    
+    def getRecommendations(self):
+        return self.__recommendations__[['article_id', 'title', 'Keywords Similarity']]
+    
+    def getRecommendationsDetails(self):
+        return self.__recommendations__
+    
+    def __getRecommendations__(self):        
+        self.__recommendations__ = pd.DataFrame()
+        if max(self.__similarUsersID__['Articles Similarity']) > 3:            
+            for idx in self.__similarUsersID__['Similar_Users'].index:
+                v_similarUser = self.__similarUsersID__.loc[idx, 'Similar_Users']
+                v_articles = self.getSimilarUsersDetails(v_similarUser).getUserArticles()
+                
+                if len(self.__keywords__) != 0:
+                    v_articles['Keywords Similarity'] = 0
+                    for keyword in self.__keywords__:
+                        v_articles['Keywords Similarity'] = v_articles['Keywords Similarity'] \
+                                                             + v_articles['topic_keywords'].apply(lambda x: 1 if keyword in x else 0)
+                else:
+                    v_articles['Keywords Similarity'] = 0
+                
+                v_new_art = list(np.setdiff1d( v_articles['article_id'].tolist(), 
+                                               self.__User__.getUserArticles()['article_id'].tolist(), 
+                                               assume_unique = True ))   
+                v_articles = v_articles.rename(columns = {'user_id': 'User ID'})
+                v_articles = v_articles.set_index('article_id').loc[v_new_art, :].reset_index()
+                v_articles['User Articles Similarity']   = self.__similarUsersID__.loc[idx, 'Articles Similarity']
+                v_articles['User Articles Read']         = self.getSimilarUsersDetails(v_similarUser).getUserArticlesNo()
+                v_articles['User Articles Interactions'] = self.getSimilarUsersDetails(v_similarUser).getUserArticlesInteract()
+                v_articles = v_articles.sort_values( [ 'Keywords Similarity',
+                                                       'User Articles Similarity', 
+                                                       'User Articles Read',
+                                                       'User Articles Interactions' ],
+                                                     ascending = [False, False, False, False] )
+                
+                self.__recommendations__ = pd.concat([self.__recommendations__, v_articles.head(self.__artPerUser__)])
+                self.__recommendations__.reset_index(drop = True, inplace = True)
+                if self.__recommendations__.shape[0] > self.__recNumber__ * 4: 
+                    break
+            
+            v_articles = self.__recommendations__.copy()
+            v_articles = v_articles.sort_values( [ 'article_id',
+                                                   'Keywords Similarity',
+                                                   'User Articles Similarity', 
+                                                   'User Articles Read',
+                                                   'User Articles Interactions' ],
+                                                 ascending = [True, False, False, False, False] )
+            v_articles['Duplicate'] = v_articles.shift(1)['article_id'].fillna(-999)
+            v_articles = v_articles[ v_articles['article_id'] != v_articles['Duplicate']]
+            self.__recommendations__ = v_articles.drop('Duplicate', axis = 1).copy()
+                    
+        else:
+            v_articles = self.__topArticles__.copy()
+            
+            if len(self.__keywords__) != 0:
+                v_articles['Keywords Similarity'] = 0
+                for keyword in self.__keywords__:
+                    v_articles['topic_keywords_'] = v_articles['topic_keywords'].apply(lambda x: 1 if keyword in x else 0)
+                    v_articles['Keywords Similarity'] = v_articles['Keywords Similarity'] + v_articles['topic_keywords_']
+                        
+            else:
+                v_articles['Keywords Similarity'] = 0
+                
+            v_articles['User ID']                    = np.NaN
+            v_articles['User Articles Similarity']   = np.NaN
+            v_articles['User Articles Read']         = np.NaN
+            v_articles['User Articles Interactions'] = np.NaN
+            
+            self.__recommendations__ = v_articles.copy()
+        
+        self.__recommendations__ = ( self.__recommendations__.sort_values( [ 'Keywords Similarity',
+                                                                             'User Articles Similarity', 
+                                                                             'User Articles Read', 
+                                                                             'Article Read No' ], 
+                                                                           ascending = [False, False, False, False] )
+                                                             .reset_index(drop = True)
+                                                             .head(self.__recNumber__) )        
+        return self.__recommendations__
+            
+    def __setSimilarUsers__(self, p_articles, p_userArticles, p_userArticlesMatrix, p_top):
+        v_userID = self.__User__.getUserID()
+        
+        v_userArticlesMatrix = p_userArticlesMatrix.fillna(0)
+
+        v_userArticleGroup = p_userArticles.groupby(['user_id', 'article_id']).agg({'user_id': ['count']}).reset_index()
+        v_userArticleGroup.columns = ['user_id', 'article_id', 'Article Read No']
+        v_userArticleGroup = v_userArticleGroup.groupby(['user_id']).agg({'Article Read No': ['count', 'sum']})
+        v_userArticleGroup.columns = ['Articles Read', 'Articles Interactions']
+        
+        v_similarUsers = pd.DataFrame(v_userArticlesMatrix.dot(v_userArticlesMatrix.T).loc[v_userID, :])
+        v_similarUsers.drop(v_userID, axis = 0, inplace = True)
+        v_similarUsers.reset_index(inplace = True)
+        v_similarUsers.columns = ['Similar_Users', 'Articles Similarity']
+
+        v_similarUsers = v_similarUsers.merge( v_userArticleGroup.reset_index(), 
+                                               how = 'inner', left_on = 'Similar_Users', right_on = 'user_id' )
+        
+        v_similarUsers = ( v_similarUsers.sort_values( ['Articles Similarity', 'Articles Read', 'Articles Interactions'], 
+                                                       ascending = [False, False, False])
+                                         .reset_index(drop = True)         
+                                         .head(p_top) )    
+        
+        self.__similarUsersID__  = v_similarUsers[['Similar_Users', 'Articles Similarity']]
+        self.__similarUsersDet__ = {}
+        for idx in v_similarUsers.index:
+            v_key = v_similarUsers.loc[idx, 'Similar_Users']
+            self.__similarUsersDet__[v_key] = User( p_user_id            = v_key, 
+                                                    p_articles           = p_articles, 
+                                                    p_userArticles       = p_userArticles, 
+                                                    p_userArticlesMatrix = p_userArticlesMatrix )
+        
+        return
